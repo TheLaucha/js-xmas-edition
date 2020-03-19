47cb1129e8bb8b75ca8020848d98e9a79aff8c51
TheLaucha <47804695+TheLaucha@users.noreply.github.com>
2020-03-19 12:40:14 -0300

    Arreglo de asserts
    Se arreglaron los errores que habia en los asserts

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,4 +1,4 @@
-//function probarValidarNombre(){
+function probarValidarNombre(){
   console.assert(
     validarNombre("") === "El nombre debe tener como minimo 1 caracter.", "Validar nombre no valido que el nombre sea vacio."
     );
@@ -6,26 +6,31 @@
   console.assert(
     validarNombre("111111111111111111111111111111111111111111111111111") === "El nombre debe tener menos de 50 caracteres.", "Validar nobre no valido que el nombre sea menor a 50 caracteres."
     );
-//}
+}
 
-//function probarValidarCiudad(){
+function probarValidarCiudad(){
   console.assert(
-    validarCiudad("") === "Tienes que seleccionar una ciudad", "Validar nombre no valido que la ciudad este seleccionada"
+    validarCiudad("") === "Tiene que seleccionar una ciudad", "Validar nombre no valido que la ciudad este seleccionada"
     );
-//}
+}
 
-//function probarValidarComportamiento(){
+function probarValidarComportamiento(){
   console.assert(
-    validarComportamiento("") === "Tienes que seleccionar como te comportaste", "Validar comportamiento no valido que el comportamiento este seleccionado"
+    validarComportamiento("") === "Tiene que seleccionar como te comportaste", "Validar comportamiento no valido que el comportamiento este seleccionado"
     );
-//}
+}
 
-//function probarValidarRegalo(){
+function probarValidarRegalo(){
   console.assert(
-    validarComportamiento("") === "Tienes que escribir como minimo 1 caracter", "Validar regalo no valido que el campo este vacio"
+    validarRegalo("") === "Este campo tiene que tener como minimo 1 caracter", "Validar regalo no valido que el campo este vacio"
     );
 
   console.assert(
-    validarComportamiento("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") === "Este campo debe tener menos de 250 caracteres", "Validar campo de regalo no valido que el campo tenga menos de 250 caraccteres"
+    validarRegalo("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") === "Este campo no puede superar los 250 caracteres.", "Validar campo de regalo no valido que el campo tenga menos de 250 caraccteres"
     );
-//}
+}
+
+probarValidarNombre();
+probarValidarCiudad();
+probarValidarComportamiento()
+probarValidarRegalo();
